a892d9fdd02fb01797f37a7f8ff72c00b866d0f0
Vineeth Madishetti <vineethmadishetti@gmail.com>
2025-11-26 03:46:25 +0530

    Fix axios to use my env variable

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 
+// This line checks if Vercel provided a URL. If not, it uses localhost.
+const apiUrl = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api', // Adjust if deployed
-  withCredentials: true, // IMPORTANT: Allows sending/receiving httpOnly cookies
+  baseURL: apiUrl,
+  withCredentials: true,
 });
 
 export default instance;
